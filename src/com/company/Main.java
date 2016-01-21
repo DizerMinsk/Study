a0032988e4dd1363e2c053b6937f6ab1fd1e090f
Mikalai Plinda <nikolay199@tut.by>
2016-01-21 14:00:25 +0300

    init my first IBA Web driver curces project

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "MIKALAI";
+        String surName = "PLINDA";
+        String city = "MINSK";
+        int age = 37;
+
+
+        System.out.println("Fullname: "+name+" "+surName);
+        System.out.println("City: "+city);
+        System.out.println("Age: "+age);
+	// write youSr code here
+    }
+}
